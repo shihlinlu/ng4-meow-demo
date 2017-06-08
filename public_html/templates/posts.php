d4c37c3f13c3544161d105a51b9b0936f43932e6
Shihlin Lu <shihlinlu@gmail.com>
2017-06-08 16:44:27 -0600

    added ngFor after making service call

--- public_html/templates/posts.php
+++ public_html/templates/posts.php
@@ -5,16 +5,49 @@
 		<main class="bg">
 			<div class="container">
 				<div class="row">
-					<div class="col-xs-12">
-						<h1>About Meow</h1>
+
+					<div class="col-md-4">
+						<h1>Create New Post</h1>
+
+						<!-- Create New Post Form -->
+						<form id="contact-form">
+							<div class="form-group">
+								<label class="sr-only" for="postTitle">Title <span class="text-danger">*</span></label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-pencil" aria-hidden="true"></i>
+									</div>
+									<input type="text" class="form-control" id="postTitle" name="postTitle" placeholder="Post Title">
+								</div>
+							</div>
+							<div class="form-group">
+								<label class="sr-only" for="postContent">Content <span class="text-danger">*</span></label>
+								<div class="input-group">
+									<div class="input-group-addon">
+										<i class="fa fa-envelope" aria-hidden="true"></i>
+									</div>
+									<textarea class="form-control" name="postContent" id="postContent" cols="30" rows="10" placeholder="1024 characters max."></textarea>
+								</div>
+							</div>
+
+							<button class="btn btn-success" type="submit"><i class="fa fa-paper-plane"></i> Submit</button>
+							<button class="btn btn-warning" type="reset"><i class="fa fa-ban"></i> Reset</button>
+						</form>
+
 					</div>
-				</div>
-				<div class="row">
-					<div class="col-md-6">
-						<p>Meow for food, then when human fills food dish, take a few bites of food and continue meowing fall asleep on the washing machine hunt by meowing loudly at 5am next to human slave food dispenser. Rub face on owner lies down but you call this cat food?. Kick up litter put butt in owner's face and wake up wander around the house making large amounts of noise jump on top of your human's bed and fall asleep again and eats owners hair then claws head.</p>
-					</div>
-					<div class="col-md-6">
-						<p>When in doubt, wash eat prawns daintily with a claw then lick paws clean wash down prawns with a lap of carnation milk then retire to the warmest spot on the couch to claw at the fabric before taking a catnap hunt anything that moves, and purr while eating pee in human's bed until he cleans the litter box inspect anything brought into the house purr.</p>
+
+					<div class="col-md-8">
+						<h1>Blabbermouth Kitty Diary</h1>
+
+						<!-- Begin Post Item -->
+						<div *ngFor="let post of posts" class="panel panel-default">
+							<div class="panel-heading">
+								<div class="h4">{{ post.postTitle }} <small>{{ post.postDate | date : "medium" }}</small></div>
+							</div>
+							<div class="panel-body">
+								{{post.postContent }}
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
